715a0a27afc7d08af3c53df318fcd9307fb0f839
Djordje Palavestra <djordjepalavestra@gmail.com>
2023-04-26 08:28:32 +0200

    second

---
+++ MusicApi/MusicApi/Controllers/SongsController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MusicApi.Data;
+using MusicApi.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MusicApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SongsController : ControllerBase
+    {
+        private readonly ApiDbContext _dbContext;
+        public SongsController(ApiDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        // GET: api/<SongsController>
+        [HttpGet]
+        public IEnumerable<Song> Get()
+        {
+            return _dbContext.Songs;
+        }
+
+        // GET api/<SongsController>/5
+        [HttpGet("{id}")]
+        public Song Get(int id)
+        {
+            var song = _dbContext.Songs.Find(id);//.FirstOrDefault(x => x.Id == id);
+            return song;
+        }
+
+        // POST api/<SongsController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<SongsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<SongsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- MusicApi/MusicApi/Program.cs
+++ MusicApi/MusicApi/Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.EntityFrameworkCore;
+using MusicApi.Data;
+
 namespace MusicApi
 {
     public class Program
@@ -9,10 +12,13 @@ namespace MusicApi
 
             // Add services to the container.
 
+
+
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddDbContext<ApiDbContext>(option => option.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=MusicDb;"));
 
             var app = builder.Build();
 
@@ -21,6 +27,13 @@ namespace MusicApi
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
+            }
+
+            using (var scope = app.Services.CreateScope())
+            {
+                var dbContext = scope.ServiceProvider.GetRequiredService<ApiDbContext>();
+                // use context
+                dbContext.Database.EnsureCreated(); // check if database exist, if exist than do not create database, otherwise create database
             }
 
             app.UseHttpsRedirection();
